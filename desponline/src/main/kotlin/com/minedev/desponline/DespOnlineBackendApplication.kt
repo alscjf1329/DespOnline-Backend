76727ba055ec817d154d58ad3eaa8e7e144fa02b
alscjf1329 <alscjsf1329@gmail.com>
2023-10-11 09:41:38 +0900

    first commit

---
+++ desponline/src/main/kotlin/com/minedev/desponline/DespOnlineBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.minedev.desponline
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DespOnlineBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<DespOnlineBackendApplication>(*args)
+}
---
+++ desponline/src/test/kotlin/com/minedev/desponline/DespOnlineBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.minedev.desponline
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DespOnlineBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
